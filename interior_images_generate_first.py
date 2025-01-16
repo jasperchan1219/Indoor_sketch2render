919e693cfd4de2ded54cdf1e612be0001e744588
jasperchan1219 <jasperchan@caece.net>
2025-01-16 16:21:04 +0800

    commit all

---
+++ interior_images_generate_first.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+import time
+import urllib
+import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+# 存圖位置
+local_path = './data/interior-styles/japanese'
+# 頁面網址（在google圖片搜尋關鍵字後複製網址）
+url = 'https://www.google.com/search?q=japanese+style+interior+design&sxsrf=APwXEdcAOzrQXkcqBIFJaaWJxB0RPUMbmw:1682679434902&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiDv_Ovtcz-AhWst1YBHf13B9EQ_AUoAXoECAEQAw&biw=1512&bih=834&dpr=2'
+chromeDriver = r'/usr/local/bin/chromedriver.exe' # chromedriver檔案放的位置，要先到chrome的設定->關於chrome 看自己的chrome版本，並下載對應的chromedriver.exe到/usr/local/bin/，下載網址：https://chromedriver.chromium.org/downloads
+driver = webdriver.Chrome(service=Service(chromeDriver)) 
+  
+# 最大化窗口，因為每一次爬取只能看到視窗内的圖片
+driver.maximize_window()  
+  
+# 紀錄下載過的圖片網址，避免重複下載  
+img_url_dic = {}  
+
+# 瀏覽器打開爬取頁面
+driver.get(url)  
+
+# 模擬滾動視窗瀏覽更多圖片
+pos = 0  
+m = 0 # 圖片編號 
+for i in range(100):  
+    pos += i*500 # 每次下滾500  
+    js = "document.documentElement.scrollTop=%d" % pos  
+    driver.execute_script(js)  
+    time.sleep(1)
+    for j in range(100):
+        xpath = '//*[@id="islrg"]/div[1]/div['+str(j)+']/a[1]/div[1]/img'
+        for element in driver.find_elements(By.XPATH,xpath):
+            try:
+                img_url = element.get_attribute('src')
+                # 保存圖片到指定路徑
+                if img_url != None and not img_url in img_url_dic:
+                    img_url_dic[img_url] = ''  
+                    m += 1
+                    # print(img_url)
+                    # ext = img_url.split('/')[-1]
+                    # print(ext)
+                    # filename = str(m) + 'interior' + '_' + ext +'.jpg'
+                    filename = 'japanese'+str(m)+'.jpg'
+                    print(filename)
+                    # 保存圖片
+                    urllib.request.urlretrieve(img_url, os.path.join(local_path , filename))
+            except OSError:
+                print('發生OSError!')
+                print(pos)
+                break
+            
+            
+driver.close()
---
+++ neural-style-transfer/neural_style_transfer_multi.py
@@ -0,0 +1,123 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from PIL import Image
+import torchvision.transforms as transforms
+import torchvision.models as models
+from torchvision.utils import save_image
+import os
+
+gpu_msg = torch.has_mps
+if gpu_msg:
+    print("using mps")
+else:
+    print("not using mps")
+
+class VGG(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.chosen_features = ['0', '5', '10', '19', '28']
+        self.model = models.vgg19(pretrained=True).features[:29]
+
+    def forward(self, x):
+        features = []
+
+        for layer_num, layer in enumerate(self.model):
+            x = layer(x)
+
+            if str(layer_num) in self.chosen_features:
+                features.append(x)
+
+        return features
+    
+
+
+def load_image(image_name, transform):
+    image = Image.open(image_name)
+    image = image.convert('RGB')
+    image = transform(image).unsqueeze(0)
+    return image
+
+
+
+def train(content_img_path, style_img_path, output_path):
+    device = torch.device("mps" if torch.has_mps else "cpu")
+    image_size = 256
+
+    transform = transforms.Compose(
+        [
+            transforms.Resize((image_size, image_size)),
+            transforms.ToTensor(),
+        ]
+    )
+
+    content_img = load_image(content_img_path, transform).to(device)
+    style_img = load_image(style_img_path, transform).to(device)
+
+    model = VGG().to(device)
+    model.eval()
+
+    # Randomly generated image, but usually copy from the orignal image
+    # yield better result.
+    # generated = torch.randn(content_img.shape, device=device, requires_grad=True)
+    generated = content_img.clone().requires_grad_(True)
+
+    # Hyperparameters
+    total_steps = 100000
+    learning_rate = 0.001
+    alpha = 1
+    beta = 0.001
+    optimizer = optim.Adam([generated], lr=learning_rate)
+
+    for step in range(total_steps):
+        generated_features = model(generated)
+        content_img_features = model(content_img)
+        style_features = model(style_img)
+
+        content_loss = 0
+        style_loss = 0
+
+        for gen_feature, content_feature, style_feature in zip(
+            generated_features, content_img_features, style_features
+        ):
+            batch_size, channel, height, width = gen_feature.shape
+
+            # content loss
+            content_loss += torch.mean((gen_feature - content_feature) ** 2)
+
+            # style loss: compute gram matrix
+            G = gen_feature.view(channel, height*width).mm(
+                gen_feature.view(channel, height*width).t()
+            )
+            A = style_feature.view(channel, height*width).mm(
+                style_feature.view(channel, height*width).t()
+            )
+            style_loss += torch.mean((G - A) ** 2)
+
+        total_loss = alpha * content_loss + beta * style_loss
+        optimizer.zero_grad()
+        total_loss.backward()
+        optimizer.step()
+
+        if step % 100 == 0:
+            print(f"step: {step}, content loss: {content_loss:6.3f}, style loss: {style_loss:6.3f}, total loss: {total_loss:6.3f}", flush=True)
+        if step % 1000 == 0:
+            save_image(generated, f"{output_path}/step_{step}.png")
+
+
+if __name__ == "__main__":
+    content_img_path = "./data/interior/sketch_images/2point_1.png"
+
+    # for style_index in range(1, 9):
+    #     print("Generating style:", style_index)
+    #     style_img_path = f"data/interior-styles/japanese/japanese{style_index}.jpg"
+    #     output_path = f"data/japanese_style_{style_index}"
+    #     if not os.path.exists(output_path):
+    #         os.mkdir(output_path)
+    #     train(content_img_path, style_img_path, output_path)
+    style_img_path = f"data/interior-styles/japanese/japanese1.jpg"
+    output_path = f"data/japanese_style_1"
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    train(content_img_path, style_img_path, output_path)
